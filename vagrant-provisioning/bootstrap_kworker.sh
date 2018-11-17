3f145b43155485c511a27511d31f5f77e829aec9
Venkat Nagappan <wenkatn@gmail.com>
2018-11-17 16:02:27 +0000

    updated bootstrap script

--- vagrant-provisioning/bootstrap_kworker.sh
+++ vagrant-provisioning/bootstrap_kworker.sh
@@ -4,4 +4,4 @@
 echo "[TASK 1] Join node to Kubernetes Cluster"
 yum install -q -y sshpass >/dev/null 2>&1
 sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh
-bash /joincluster.sh
+bash /joincluster.sh >/dev/null 2>&1
