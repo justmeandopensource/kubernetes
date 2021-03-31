9e27be2b4a01d49e9a73dde4f9423796d03c15df
srdrcn <you@example.com>
2021-03-31 16:17:38 +0300

    only vagrant

--- docs/cheatsheet.md
+++
@@ -1,8 +0,0 @@
-List Master nodes
-```
-kubectl get nodes --selector='node-role.kubernetes.io/master'
-```
-List worker nodes
-```
-kubectl get nodes --selector='!node-role.kubernetes.io/master'
-```
--- vagrant-provisioning/bootstrap_kworker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "[TASK 1] Join node to Kubernetes Cluster"
-apt install -qq -y sshpass >/dev/null 2>&1
-sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
-bash /joincluster.sh >/dev/null 2>&1
