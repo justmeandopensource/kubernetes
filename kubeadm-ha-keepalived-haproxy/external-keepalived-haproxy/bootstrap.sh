c65192673082d868689ed0b3e67154b2233909d0
Venkat Nagappan <wenkatn@gmail.com>
2021-10-02 19:06:05 +0100

    initial ha docs

---
+++ kubeadm-ha-keepalived-haproxy/external-keepalived-haproxy/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "[TASK 1] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 2] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
