2d345ebbd4e22e1c7e6211778bc69487e758b68c
gstanden <gilstanden@hotmail.com>
2021-11-15 09:33:07 -0600

    v1.0.1-alpha

--- lxd-provisioning/cont-centos8-5.sh
+++ lxd-provisioning/cont-centos8-5.sh
@@ -1,213 +1,61 @@
 #!/bin/bash
+#
+#    Copyright 2015-2021 Gilbert Standen
+#    This file is part of Orabuntu-LXC.
 
-echo ''
-echo "==============================================" 
-echo "Install EPEL...                               "
-echo "=============================================="
-echo ''
+#    Orabuntu-LXC is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-n=1
-Cmd0=1
-while [ $Cmd0 -ne 0 ] && [ $n -lt 5 ]
-do
-	dnf install -y epel-releasea
-	Cmd0=`echo $?`
-	n=$((n+1))
-	sleep 5
-done
+#    Orabuntu-LXC is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 
-echo ''
-echo "==============================================" 
-echo "Done: Install EPEL.                           "
-echo "=============================================="
-echo ''
+#    You should have received a copy of the GNU General Public License
+#    along with Orabuntu-LXC.  If not, see <http://www.gnu.org/licenses/>.
 
-sleep 5
+#    v2.4 		GLS 20151224
+#    v2.8 		GLS 20151231
+#    v3.0 		GLS 20160710 Updates for Ubuntu 16.04
+#    v4.0 		GLS 20161025 DNS DHCP services moved into an LXC container
+#    v5.0 		GLS 20170909 Orabuntu-LXC Multi-Host
+#    v6.0-AMIDE-beta	GLS 20180106 Orabuntu-LXC AmazonS3 Multi-Host Docker Enterprise Edition (AMIDE)
+#    v7.0-ELENA-beta    GLS 20210428 Enterprise LXD Edition New AMIDE
 
-clear
+#    Note that this software builds a containerized DNS DHCP solution (bind9 / isc-dhcp-server).
+#    The nameserver should NOT be the name of an EXISTING nameserver but an arbitrary name because this software is CREATING a new LXC-containerized nameserver.
+#    The domain names can be arbitrary fictional names or they can be a domain that you actually own and operate.
+#    There are two domains and two networks because the "seed" LXC containers are on a separate network from the production LXC containers.
+#    If the domain is an actual domain, you will need to change the subnet using the subnets feature of Orabuntu-LXC
 
-echo ''
-echo "==============================================" 
-echo "Install sshpass...                            "
-echo "=============================================="
-echo ''
-
-n=1
-Cmd1=1
-while [ $Cmd1 -ne 0 ] && [ $n -lt 5 ]
-do
-	dnf install -y sshpass
-	Cmd1=`echo $?`
-	n=$((n+1))
-	sleep 5
-done
-
-echo ''
-echo "==============================================" 
-echo "Install sshpass...                            "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Enable kubelet service...                     "
-echo "=============================================="
-echo ''
-
+#!/bin/bash
+dnf install -y epel-release
+dnf install -y sshpass
 systemctl enable kubelet.service
-
-echo ''
-echo "==============================================" 
-echo "Done: Enable kubelet service.                 "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Turn off swap...                              "
-echo "=============================================="
-echo ''
-
-sudo swapoff -a
-sudo free -m
-
-echo ''
-echo "==============================================" 
-echo "Done: Turn off swap.                          "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Run kubeadm...                                "
-echo "=============================================="
-echo ''
-
 kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.23.0-alpha.3 | tee kubeadm_init.log
-
-echo ''
-echo "==============================================" 
-echo "Done: Run kubeadm.                            "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
 echo "Sleeping 30 seconds while kubernetes master starts running ..."
 sleep 30
-
-echo ''
-echo "==============================================" 
-echo "Configure environment...                      "
-echo "=============================================="
-echo ''
-
-export KUBECONFIG=/etc/kubernetes/admin.conf
-echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> ~/.bash_profile
-cat /etc/kubernetes/admin.conf
-
-echo ''
-echo "==============================================" 
-echo "Done: Configure environment.                  "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Install weavenet...                           "
-echo "=============================================="
-echo ''
-
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-echo ''
-echo "==============================================" 
-echo "Done: Install weavenet.                       "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Monitor controller startup status...          "
-echo "=============================================="
-echo ''
-
-echo "Sleeping 30 seconds while cluster starts up ..."
-echo ''
-
-kubectl get pods --all-namespaces -o wide
-
-sleep 20
-
-echo "Sleeping 30 seconds while cluster starts up ..."
-echo ''
-
-echo ''
-kubectl get pods --all-namespaces -o wide
-
-sleep 20
-
-echo "Sleeping 30 seconds while cluster starts up ..."
-echo ''
-
-kubectl get pods --all-namespaces -o wide
-sleep 20
-
-echo ''
-echo "==============================================" 
-echo "Done: Monitor controller startup status.      "
-echo "=============================================="
-echo ''
-
-sleep 5
-
-clear
-
-echo ''
-echo "==============================================" 
-echo "Join cluster nodes...                         "
-echo "=============================================="
-echo ''
-
 cat kubeadm_init.log | grep -A1 join | grep -A1 token > joincluster.sh
 chmod +x joincluster.sh
-
-sshpass -p root scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /root/joincluster.sh root@10.207.39.5:/root/.
-sshpass -p root scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /root/joincluster.sh root@10.207.39.6:/root/.
-
+export KUBECONFIG=/etc/kubernetes/admin.conf
+echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> ~/.bash_profile
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+echo "Sleeping 30 seconds while weavenet starts running ..."
 echo ''
-sshpass -p root ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no root@10.207.39.5 "/root/joincluster.sh"
+kubectl get pods --all-namespaces -o wide
+sleep 30
+echo ''
+kubectl get pods --all-namespaces -o wide
+sleep 30
+echo ''
+kubectl get pods --all-namespaces -o wide
+sleep 5
+sshpass -p root scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /root/joincluster.sh root@10.209.53.5:/root/.
+sshpass -p root scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /root/joincluster.sh root@10.209.53.6:/root/.
+echo ''
+sshpass -p root ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no root@10.209.53.5 "/root/joincluster.sh"
 sleep 10
-
 echo ''
-sshpass -p root ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no root@10.207.39.6 "/root/joincluster.sh"
-sleep 10
-
-echo ''
-echo "==============================================" 
-echo "Done: Join cluster nodes.                     "
-echo "=============================================="
-echo ''
-
+sshpass -p root ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no root@10.209.53.6 "/root/joincluster.sh"
