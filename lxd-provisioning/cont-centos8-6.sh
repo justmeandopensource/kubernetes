d629b4cdcff0d699850fa45826b7e540ef68ac1a
gstanden <gilstanden@hotmail.com>
2021-11-19 18:58:22 -0600

    v1.0.1-alpha

--- lxd-provisioning/cont-centos8-6.sh
+++ lxd-provisioning/cont-centos8-6.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
 # Deploy metallb load balancer
-
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl get ns
 sleep 5
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
-
 function CountRunningMetallb {
 	kubectl get all -n metallb-system | grep -c Running
 }
 RunningMetallb=$(CountRunningMetallb)
-
 kubectl create configmap config --from-file=/root/configmap
 kubectl describe configmap config
 
@@ -19,27 +17,27 @@ kubectl describe configmap config
 # kubectl expose deploy nginx --port 80 --type LoadBalancer
 
 # Deploy ingress-nginx
-
+kubectl create ns ingress-nginx
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
-./get-helm.sh
+./get_helm.sh
 
 helm version --short
-
+echo ''
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-help repo update
+echo ''
+helm repo update
+echo ''
 helm repo list
+echo ''
 helm search repo ingress
-
-# helm show values ingress-nginx/ingress-nginx > ingress-nginx.yaml (this step will be replaced by my customized file in the archive so that it does not need the three edits)
-
-# lxc file push ingress-nginx.yaml -- maestro/root/
-
+echo ''
 helm install orabuntu-lxd ingress-nginx/ingress-nginx -n ingress-nginx --values /root/ingress-nginx.yaml
-
+echo ''
 helm list -n ingress-nginx
-
+echo ''
 kubectl -n ingress-nginx get all
+echo ''
 
 # sudo iptables -t nat -A PREROUTING -p tcp -i enp0s17 --dport 80 -j DNAT --to-destination 10.209.53.240:80
 
--- lxd-provisioning/cont-centos8-push.sh
+++ lxd-provisioning/cont-centos8-push.sh
@@ -1,4 +1,4 @@
 eval echo "'/var/lib/snapd/snap/bin/lxc file push cont-centos8-[01234567].sh maestro/root/' | sg lxd $CGROUP_SUFFIX"
-eval echo "'/var/lib/snapd/snap/bin/lxc file push cont-centos8-[012346].sh violin1/root/'  | sg lxd $CGROUP_SUFFIX"
-eval echo "'/var/lib/snapd/snap/bin/lxc file push cont-centos8-[012346].sh violin2/root/'  | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc file push cont-centos8-[012346].sh violin1/root/'   | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc file push cont-centos8-[012346].sh violin2/root/'   | sg lxd $CGROUP_SUFFIX"
 
