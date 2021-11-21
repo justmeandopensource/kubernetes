38c3b9a5c72983028f294044c2976251d014f9d4
gstanden <gilstanden@hotmail.com>
2021-11-21 08:36:03 -0600

    v1.0.1-alpha

--- lxd-provisioning/host-oracle8-modules-remove.sh
+++ lxd-provisioning/host-oracle8-modules-remove.sh
@@ -2,17 +2,57 @@
 
 echo ''
 echo "==============================================" 
+echo "Stop k8s containers...                        "
+echo "=============================================="
+echo ''
+
+eval echo "'/var/lib/snapd/snap/bin/lxc stop maestro -f' | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc stop violin1 -f' | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc stop violin2 -f' | sg lxd $CGROUP_SUFFIX"
+
+echo ''
+echo "==============================================" 
+echo "Done: Stop k8s containers...                        "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "==============================================" 
 echo "Unmount /lib/modules in container...          "
 echo "=============================================="
 echo ''
 
-eval echo "'/var/lib/snapd/snap/bin/lxc config device remove maestro  libs' | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc config device remove maestro libs' | sg lxd $CGROUP_SUFFIX"
 eval echo "'/var/lib/snapd/snap/bin/lxc config device remove violin1 libs' | sg lxd $CGROUP_SUFFIX"
 eval echo "'/var/lib/snapd/snap/bin/lxc config device remove violin2 libs' | sg lxd $CGROUP_SUFFIX"
 
 echo ''
 echo "==============================================" 
 echo "Done: Unmount /lib/modules in container.      "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "==============================================" 
+echo "Stop k8s containers...                        "
+echo "=============================================="
+echo ''
+
+eval echo "'/var/lib/snapd/snap/bin/lxc start maestro' | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc start violin1' | sg lxd $CGROUP_SUFFIX"
+eval echo "'/var/lib/snapd/snap/bin/lxc start violin2' | sg lxd $CGROUP_SUFFIX"
+
+echo ''
+echo "==============================================" 
+echo "Done: Stop k8s containers...                        "
 echo "=============================================="
 echo ''
 
