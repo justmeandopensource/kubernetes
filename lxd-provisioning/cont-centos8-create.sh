e54ef59b44178c4ed8221e8d4095a15d475ae842
gstanden <gilstanden@hotmail.com>
2022-03-28 13:39:02 +0000

    v1.0.7-alpha

--- lxd-provisioning/cont-centos8-create.sh
+++ lxd-provisioning/cont-centos8-create.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+#
+#    Copyright 2015-2021 Gilbert Standen
+#    This file is part of Orabuntu-LXC.
+
+#    Orabuntu-LXC is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Orabuntu-LXC is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Orabuntu-LXC.  If not, see <http://www.gnu.org/licenses/>.
+
+#    v2.4 		GLS 20151224
+#    v2.8 		GLS 20151231
+#    v3.0 		GLS 20160710 Updates for Ubuntu 16.04
+#    v4.0 		GLS 20161025 DNS DHCP services moved into an LXC container
+#    v5.0 		GLS 20170909 Orabuntu-LXC Multi-Host
+#    v6.0-AMIDE-beta	GLS 20180106 Orabuntu-LXC AmazonS3 Multi-Host Docker Enterprise Edition (AMIDE)
+#    v7.0-ELENA-beta    GLS 20210428 Enterprise LXD Edition New AMIDE
+
+#    Note that this software builds a containerized DNS DHCP solution (bind9 / isc-dhcp-server).
+#    The nameserver should NOT be the name of an EXISTING nameserver but an arbitrary name because this software is CREATING a new LXC-containerized nameserver.
+#    The domain names can be arbitrary fictional names or they can be a domain that you actually own and operate.
+#    There are two domains and two networks because the "seed" LXC containers are on a separate network from the production LXC containers.
+#    If the domain is an actual domain, you will need to change the subnet using the subnets feature of Orabuntu-LXC
+
+#!/bin/bash
 
 NameServer=$1
 Domain1=$2
@@ -103,143 +135,58 @@ then
 
 elif [ $GRE = 'Y' ]
 then
-	ViolinIndex=3
+        ViolinIndex=1
 
-	function CheckDNSLookup {
-        	timeout 10 nslookup violin"$ViolinIndex" $NameServer
-	}
-	DNSLookup=$(CheckDNSLookup)
-	DNSLookup=`echo $?`
+        function CheckDNSLookup {
+                timeout 10 nslookup violin"$ViolinIndex" $NameServer
+        }
+        DNSLookup=$(CheckDNSLookup)
+        DNSLookup=`echo $?`
 
-	while [ $DNSLookup -eq 0 ]
-	do
-        	ViolinIndex=$((ViolinIndex+1))
-        	DNSLookup=$(CheckDNSLookup)
-        	DNSLookup=`echo $?`
-	done
+        while [ $DNSLookup -eq 0 ]
+        do
+                ViolinIndex=$((ViolinIndex+1))
+                DNSLookup=$(CheckDNSLookup)
+                DNSLookup=`echo $?`
+        done
 
-	i=violin$ViolinIndex
+        i=violin$ViolinIndex
 
-	eval echo "'/var/lib/snapd/snap/bin/lxc stop -f $i ' | sg lxd $CGROUP_SUFFIX" >/dev/null 2>&1
-	eval echo "'/var/lib/snapd/snap/bin/lxc delete $i '  | sg lxd $CGROUP_SUFFIX" >/dev/null 2>&1
+        eval echo "'/var/lib/snapd/snap/bin/lxc stop -f $i ' | sg lxd $CGROUP_SUFFIX" >/dev/null 2>&1
+        eval echo "'/var/lib/snapd/snap/bin/lxc delete $i '  | sg lxd $CGROUP_SUFFIX" >/dev/null 2>&1
 
-	Status=1
-	n=1
-	while [ $Status -ne 0 ] && [ $n -le 5 ]
-	do
-		eval echo "'/var/lib/snapd/snap/bin/lxc init images:centos/8-Stream $i --profile k8s-weavenet' | sg lxd $CGROUP_SUFFIX"
-		Status=`echo $?`
-		n=$((n+1))
-		sleep 5
-	done
-	
-	function GetHwaddr {
-		eval echo "'/var/lib/snapd/snap/bin/lxc config show $i | grep hwaddr | rev | cut -c1-17 | rev' | sg lxd $CGROUP_SUFFIX"
-	}
-	Hwaddr=$(GetHwaddr)
+        Status=1
+        n=1
+        while [ $Status -ne 0 ] && [ $n -le 5 ]
+        do
+                echo "Create container $i"
+                eval echo "'/var/lib/snapd/snap/bin/lxc init images:centos/8-Stream $i --profile k8s-weavenet' | sg lxd $CGROUP_SUFFIX"
+                Status=`echo $?`
+                n=$((n+1))
+                echo $n
+                sleep 5
+        done
 
-	function GetIpaddr {
-		lxc info $i | grep inet | grep -v inet6 | grep global | rev | cut -f2 -d' ' | rev | cut -f1 -d'/'
-	}
-	Ipaddr=$(GetIpaddr)
+        Status=1
+        n=1
+        while [ $Status -ne 0 ] && [ $n -le 5 ]
+        do
+                echo "Install packages in container $i"
+                eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- dnf -y install openssh-server net-tools bind-utils git rsync' | sg lxd $CGROUP_SUFFIX"
+                Status=`echo $?`
+                n=$((n+1))
+                echo $n
+                sleep 5
+        done
 
-	function GetIpaddr2 {
-		lxc info $i | grep inet | grep -v inet6 | grep global | rev | cut -f2 -d' ' | rev | cut -f1 -d'/' | cut -f4 -d'.'
-	}
-	Ipaddr2=$(GetIpaddr2)
+        eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- systemctl enable sshd' | sg lxd $CGROUP_SUFFIX"
+        eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- service sshd start' | sg lxd $CGROUP_SUFFIX"
+        eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- usermod --password `perl -e "print crypt('root','root');"` root' | sg lxd $CGROUP_SUFFIX"
 
-	sudo lxc-attach -n $NameServer -- sudo sh -c "echo 'subclass \"black-hole\" $Hwaddr;' >> /etc/dhcp/dhcpd.conf"
-	sudo lxc-attach -n $NameServer -- sudo service isc-dhcp-server restart
-
-	eval echo "'/var/lib/snapd/snap/bin/lxc start $i' | sg lxd $CGROUP_SUFFIX"
-	
-	sed -i "s/orabuntu-lxc.com/$Domain1/g" ifcfg-eth0-$i 
-	cp ifcfg-eth0-$i ifcfg-eth0
-
-	Status=1
-	n=1
-	while [ $Status -ne 0 ] && [ $n -le 5 ]
-	do
-		eval echo "'/var/lib/snapd/snap/bin/lxc file push ifcfg-eth0 $i/etc/sysconfig/network-scripts/' | sg lxd $CGROUP_SUFFIX"
-		Status=`echo $?`
-		n=$((n+1))
-		sleep 5
-	done
-
-	eval echo "'/var/lib/snapd/snap/bin/lxc stop  $i' | sg lxd $CGROUP_SUFFIX"
-	eval echo "'/var/lib/snapd/snap/bin/lxc start $i' | sg lxd $CGROUP_SUFFIX"
-
-	Status=1
-	n=1
-	while [ $Status -ne 0 ] && [ $n -le 5 ]
-	do
-		eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- dnf -y install openssh-server net-tools bind-utils git rsync' | sg lxd $CGROUP_SUFFIX"
-		Status=`echo $?`
-		n=$((n+1))
-		sleep 5
-	done
-
-	eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- systemctl enable sshd' | sg lxd $CGROUP_SUFFIX"
-	eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- service sshd start' | sg lxd $CGROUP_SUFFIX"
-	eval echo "'/var/lib/snapd/snap/bin/lxc exec $i -- usermod --password `perl -e "print crypt('root','root');"` root' | sg lxd $CGROUP_SUFFIX"
-
-	if [ -f /sys/fs/cgroup/cgroup.controllers ]
-	then
-		eval echo "'/var/lib/snapd/snap/bin/lxc config device add "$i" "kmsg" unix-char source="/dev/kmsg" path="/dev/kmsg"' | sg lxd $CGROUP_SUFFIX"
-	fi
-
-        echo ''
-        echo "=============================================="
-        echo "Create ADD DNS $i.$Domain1...         "
-        echo "=============================================="
-        echo ''
-
-        sudo sh -c "echo 'echo \"server 10.207.39.2'                                                            >  /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo 'update add $i.orabuntu-lxc.com 3600 IN A $Ipaddr'  			                >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo 'send'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo 'update add $Ipaddr2.39.207.10.in-addr.arpa 3600 IN PTR $i.orabuntu-lxc.com'  i	>> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo 'send'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo 'quit'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-        sudo sh -c "echo '\" | nsupdate -k /etc/bind/rndc.key'                                                  >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
-
-        sudo chmod 777                                          /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
-        sudo ls -l                                              /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
-        sudo sed -i "s/orabuntu-lxc\.com/$Domain1/g"            /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
-
-        echo ''
-        echo "=============================================="
-        echo "Create DEL DNS $i.$Domain1...         "
-        echo "=============================================="
-        echo ''
-
-        sudo sh -c "echo 'echo \"server 10.207.39.2'                                    >  /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo 'update delete $i.orabuntu-lxc.com. A'                 	>> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo 'send'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo 'update delete $Ipaddr2.39.207.10.in-addr.arpa. PTR'           >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo 'send'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo 'quit'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-        sudo sh -c "echo '\" | nsupdate -k /etc/bind/rndc.key'                          >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
-
-        sudo chmod 777                                          /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
-        sudo ls -l                                              /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
-        sudo sed -i "s/orabuntu-lxc\.com/$Domain1/g"            /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
-
-        ssh-keygen -R 10.207.39.2
-        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" mkdir -p ~/Downloads"
-        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" chown ubuntu:ubuntu Downloads"
-        sshpass -p ubuntu scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /etc/network/openvswitch/nsupdate_domain1_add_$i.sh ubuntu@10.207.39.2:~/Downloads/.
-        sshpass -p ubuntu scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /etc/network/openvswitch/nsupdate_domain1_del_$i.sh ubuntu@10.207.39.2:~/Downloads/.
-        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" ~/Downloads/nsupdate_domain1_add_$i.sh"
-
-        echo ''
-        echo "=============================================="
-        echo "Done: Create ADD/DEL DNS $i.$Domain1  "
-        echo "=============================================="
-        echo ''
-
-        sleep 5
-
-        clear
+        if [ -f /sys/fs/cgroup/cgroup.controllers ]
+        then
+                eval echo "'/var/lib/snapd/snap/bin/lxc config device add "$i" "kmsg" unix-char source="/dev/kmsg" path="/dev/kmsg"' | sg lxd $CGROUP_SUFFIX"
+        fi
 fi
 
 echo ''
