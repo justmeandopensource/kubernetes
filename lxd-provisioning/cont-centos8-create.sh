5b93b88b6a9684749ef366c812ceaa8fc7fee747
gstanden <gilstanden@hotmail.com>
2022-03-28 00:33:19 +0000

    v1.0.7-alpha

--- lxd-provisioning/cont-centos8-create.sh
+++ lxd-provisioning/cont-centos8-create.sh
@@ -103,10 +103,10 @@ then
 
 elif [ $GRE = 'Y' ]
 then
-	ViolinIndex=1
+	ViolinIndex=3
 
 	function CheckDNSLookup {
-        	timeout 5 nslookup violin"$ViolinIndex" $NameServer
+        	timeout 10 nslookup violin"$ViolinIndex" $NameServer
 	}
 	DNSLookup=$(CheckDNSLookup)
 	DNSLookup=`echo $?`
@@ -137,6 +137,16 @@ then
 		eval echo "'/var/lib/snapd/snap/bin/lxc config show $i | grep hwaddr | rev | cut -c1-17 | rev' | sg lxd $CGROUP_SUFFIX"
 	}
 	Hwaddr=$(GetHwaddr)
+
+	function GetIpaddr {
+		lxc info $i | grep inet | grep -v inet6 | grep global | rev | cut -f2 -d' ' | rev | cut -f1 -d'/'
+	}
+	Ipaddr=$(GetIpaddr)
+
+	function GetIpaddr2 {
+		lxc info $i | grep inet | grep -v inet6 | grep global | rev | cut -f2 -d' ' | rev | cut -f1 -d'/' | cut -f4 -d'.'
+	}
+	Ipaddr2=$(GetIpaddr2)
 
 	sudo lxc-attach -n $NameServer -- sudo sh -c "echo 'subclass \"black-hole\" $Hwaddr;' >> /etc/dhcp/dhcpd.conf"
 	sudo lxc-attach -n $NameServer -- sudo service isc-dhcp-server restart
@@ -177,6 +187,59 @@ then
 	then
 		eval echo "'/var/lib/snapd/snap/bin/lxc config device add "$i" "kmsg" unix-char source="/dev/kmsg" path="/dev/kmsg"' | sg lxd $CGROUP_SUFFIX"
 	fi
+
+        echo ''
+        echo "=============================================="
+        echo "Create ADD DNS $i.$Domain1...         "
+        echo "=============================================="
+        echo ''
+
+        sudo sh -c "echo 'echo \"server 10.207.39.2'                                                            >  /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo 'update add $i.orabuntu-lxc.com 3600 IN A $Ipaddr'  			                >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo 'send'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo 'update add $Ipaddr2.39.207.10.in-addr.arpa 3600 IN PTR $i.orabuntu-lxc.com'  i	>> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo 'send'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo 'quit'                                                                                 >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+        sudo sh -c "echo '\" | nsupdate -k /etc/bind/rndc.key'                                                  >> /etc/network/openvswitch/nsupdate_domain1_add_$i.sh"
+
+        sudo chmod 777                                          /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
+        sudo ls -l                                              /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
+        sudo sed -i "s/orabuntu-lxc\.com/$Domain1/g"            /etc/network/openvswitch/nsupdate_domain1_add_$i.sh
+
+        echo ''
+        echo "=============================================="
+        echo "Create DEL DNS $i.$Domain1...         "
+        echo "=============================================="
+        echo ''
+
+        sudo sh -c "echo 'echo \"server 10.207.39.2'                                    >  /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo 'update delete $i.orabuntu-lxc.com. A'                 	>> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo 'send'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo 'update delete $Ipaddr2.39.207.10.in-addr.arpa. PTR'           >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo 'send'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo 'quit'                                                         >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+        sudo sh -c "echo '\" | nsupdate -k /etc/bind/rndc.key'                          >> /etc/network/openvswitch/nsupdate_domain1_del_$i.sh"
+
+        sudo chmod 777                                          /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
+        sudo ls -l                                              /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
+        sudo sed -i "s/orabuntu-lxc\.com/$Domain1/g"            /etc/network/openvswitch/nsupdate_domain1_del_$i.sh
+
+        ssh-keygen -R 10.207.39.2
+        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" mkdir -p ~/Downloads"
+        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" chown ubuntu:ubuntu Downloads"
+        sshpass -p ubuntu scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /etc/network/openvswitch/nsupdate_domain1_add_$i.sh ubuntu@10.207.39.2:~/Downloads/.
+        sshpass -p ubuntu scp    -o CheckHostIP=no -o StrictHostKeyChecking=no -p /etc/network/openvswitch/nsupdate_domain1_del_$i.sh ubuntu@10.207.39.2:~/Downloads/.
+        sshpass -p ubuntu ssh -t -o CheckHostIP=no -o StrictHostKeyChecking=no ubuntu@10.207.39.2 "sudo -S <<< "ubuntu" ~/Downloads/nsupdate_domain1_add_$i.sh"
+
+        echo ''
+        echo "=============================================="
+        echo "Done: Create ADD/DEL DNS $i.$Domain1  "
+        echo "=============================================="
+        echo ''
+
+        sleep 5
+
+        clear
 fi
 
 echo ''
