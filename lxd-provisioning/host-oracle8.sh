be57fb9c146c0adba5a34b94167a4de51d7f3521
gstanden <gilstanden@hotmail.com>
2022-04-21 19:25:28 +0000

    v6.13.25.16-beta

--- lxd-provisioning/host-oracle8.sh
+++ lxd-provisioning/host-oracle8.sh
@@ -3,7 +3,7 @@ LinuxFlavor=$2
 
 ./host-oracle8-firewalld.sh $LinuxFlavor
 ./host-oracle8-firewalld-check.sh $LinuxFlavor
-# ./host-oracle8-modules.sh $GRE
+./host-oracle8-modules.sh $GRE
 # ./host-oracle8-mount.sh
 ./host-oracle8-storage.sh $GRE
 
