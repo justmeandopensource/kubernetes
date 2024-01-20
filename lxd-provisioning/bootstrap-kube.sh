56a82b37ef0f48fc360f90571a8ada4360dd0fa1
Venkat Nagappan <wenkatn@gmail.com>
2024-01-20 18:31:21 +0000

    fix lxd provisioning

--- lxd-provisioning/bootstrap-kube.sh
+++ lxd-provisioning/bootstrap-kube.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-# This script has been tested on Ubuntu 20.04
+# This script has been tested on Ubuntu 22.04
 # For other versions of Ubuntu, you might need some tweaking
 
-echo "[TASK 0] Install essential packages"
-apt install -qq -y net-tools curl ssh software-properties-common >/dev/null 2>&1
+echo "[TASK 1] Install essential packages"
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -qq >/dev/null
+apt-get install -qq -y net-tools curl ssh software-properties-common >/dev/null
 
-echo "[TASK 1] Install containerd runtime"
-apt update -qq >/dev/null 2>&1
-apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
-mkdir /etc/containerd
+echo "[TASK 2] Install containerd runtime"
+apt-get install -qq -y apt-transport-https ca-certificates curl gnupg lsb-release >/dev/null
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+apt-get update -qq >/dev/null
+apt-get install -qq -y containerd.io >/dev/null
 containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 systemctl restart containerd
-systemctl enable containerd >/dev/null 2>&1
+systemctl enable containerd >/dev/null
 
-echo "[TASK 2] Add apt repo for kubernetes"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+echo "[TASK 3] Set up kubernetes repo"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' > /etc/apt/sources.list.d/kubernetes.list
 
-echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+echo "[TASK 4] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt-get update -qq >/dev/null
+apt-get install -qq -y kubeadm kubelet kubectl >/dev/null
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 systemctl restart kubelet
 
-echo "[TASK 4] Enable ssh password authentication"
+echo "[TASK 5] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
-echo "[TASK 5] Set root password"
+echo "[TASK 6] Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
 echo "export TERM=xterm" >> /etc/bash.bashrc
-
-echo "[TASK 6] Install additional packages"
-apt install -qq -y net-tools >/dev/null 2>&1
 
 #######################################
 # To be executed only on master nodes #
@@ -46,17 +52,18 @@ then
   kubeadm config images pull >/dev/null 2>&1
 
   echo "[TASK 8] Initialize Kubernetes Cluster"
-  kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+  kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
 
   echo "[TASK 9] Copy kube admin config to root user .kube directory"
   mkdir /root/.kube
-  cp /etc/kubernetes/admin.conf /root/.kube/config  
+  cp /etc/kubernetes/admin.conf /root/.kube/config
 
-  echo "[TASK 10] Deploy Flannel network"
-  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null 2>&1
+  echo "[TASK 10] Deploy Calico network"
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml >/dev/null
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml >/dev/null
 
   echo "[TASK 11] Generate and save cluster join command to /joincluster.sh"
-  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null) 
+  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null)
   echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
 
 fi
