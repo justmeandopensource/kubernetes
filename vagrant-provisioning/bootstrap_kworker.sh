b46260f99cd40888594303c79685d70c490c5289
Venkat Nagappan <45001120+justmeandopensource@users.noreply.github.com>
2024-09-30 12:30:05 +0000

    Bump k8s to 1.31 in vagrant provisioning (#145)
    * 1.31 changes to vagrant provisioning scripts
    * remove all output redirections

--- vagrant-provisioning/bootstrap_kworker.sh
+++ vagrant-provisioning/bootstrap_kworker.sh
@@ -2,6 +2,6 @@
 
 echo "[TASK 1] Join node to Kubernetes Cluster"
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -qq -y sshpass >/dev/null
-sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh >/dev/null 2>&1
+apt-get install -qq -y sshpass
+sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh
 bash /joincluster.sh >/dev/null
